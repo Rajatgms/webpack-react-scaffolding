bf509df1573c9837102151fa9b6bfba3361d6db1
rajatgms <23decrajat@gmail.com>
2020-07-05 23:17:31 +0530

    Initial webpack setup

---
+++ webpack-configs/webpack.development.js
@@ -0,0 +1,11 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = () => ({
+  mode: 'development',	
+  devtool: 'source-map',
+  devServer: {
+    open: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+});
